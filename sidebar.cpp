20f8da8c0d7d7876ba4f10f13c605ecd7bc828df
Sami Nikkanen <sami.nikkanen@tuni.fi>
2023-05-04 00:04:50 +0300

    style: beautified sidebar.cpp

--- sidebar.cpp
+++ sidebar.cpp
@@ -2,43 +2,62 @@
 #include <QFrame>
 #include <QLabel>
 
+QString createLabelStyle() {
+    return
+    "color: #9aa0a6;"
+    "font-size: 10px;"
+    "font-weight: 700;"
+    "padding: 10px;"
+    "text-transform: uppercase;"
+    "font-weight: 700;"
+    "margin-bottom: -5px;";
+}
+
 Sidebar::Sidebar(QWidget *parent)
     : QWidget(parent)
 {
     sidebarLayout = new QVBoxLayout(this);
-
     sidebarLayout->setContentsMargins(0, 0, 0, 0);
-
-    // Width of the sidebar
     setFixedWidth(250);
 
-    // Create a frame for the sidebar
     QFrame *sidebarFrame = new QFrame(this);
     sidebarFrame->setStyleSheet("background-color: #21252b;");
 
-    // Create a layout for the sidebar content
     QVBoxLayout *sidebarContentLayout = new QVBoxLayout(sidebarFrame);
     sidebarContentLayout->setContentsMargins(0, 0, 0, 0);
     sidebarContentLayout->setSpacing(0);
 
-    const QString buttonStyle = "QPushButton { text-align: left; color: #9aa0a6; background-color: #21252b; border: none; padding: 10px; font-size: 14px; font-weight: 700; }" \
-        "QPushButton:hover { background-color: #393d47 !important; }";
+    const QString buttonStyle =
+        "QPushButton {"
+        "text-align: left;"
+        "color: #9aa0a6;"
+        "background-color: #21252b;"
+        "border: none;"
+        "padding: 10px;"
+        "font-size: 14px;"
+        "font-weight: 700;}"
+        "QPushButton:hover {"
+        "background-color: #393d47 !important;}"
+    ;
 
-    // Add buttons to the sidebar
-    sidebarContentLayout->addWidget(createButton(" Toggle sidebar", QIcon(":/icons/menu.svg"), buttonStyle));
+    sidebarContentLayout->addWidget(
+        createButton(" Toggle sidebar", QIcon(":/icons/menu.svg"), buttonStyle));
 
-    // Add a label for the categories
     QLabel *filesLabel = new QLabel("Files", this);
-    filesLabel->setStyleSheet("color: #9aa0a6; font-size: 10px; font-weight: 700; padding: 10px; text-transform: uppercase; font-weight: 700; margin-bottom: -5px; ");
+    const QString labelStyle = createLabelStyle();
+
+    filesLabel->setStyleSheet(labelStyle);
+
     sidebarContentLayout->addWidget(filesLabel);
 
     allFilesButton = createButton(" All files", QIcon(":/icons/all.svg"), buttonStyle);
     connect(allFilesButton, &QPushButton::clicked, this, &Sidebar::allFilesButtonClicked);
     sidebarContentLayout->addWidget(allFilesButton);
 
-    // Add a label for the categories
     QLabel *categoriesLabel = new QLabel("Categories", this);
-    categoriesLabel->setStyleSheet("color: #9aa0a6; font-size: 10px; font-weight: 700; padding: 10px; text-transform: uppercase; font-weight: 700; margin-bottom: -5px; ");
+
+    categoriesLabel->setStyleSheet(labelStyle);
+
     sidebarContentLayout->addWidget(categoriesLabel);
 
     imagesButton = createButton(" Images", QIcon(":/icons/images.svg"), buttonStyle);
@@ -57,11 +76,8 @@ Sidebar::Sidebar(QWidget *parent)
     connect(videosButton, &QPushButton::clicked, this, &Sidebar::videosButtonClicked);
     sidebarContentLayout->addWidget(videosButton);
 
-    // Add a spacer at the bottom of the button layout to push buttons to the top
     QSpacerItem *spacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
     sidebarContentLayout->addItem(spacer);
-
-    // Add the button layout to the sidebar frame
     sidebarLayout->addWidget(sidebarFrame);
 }
 
@@ -78,8 +94,25 @@ QPushButton* Sidebar::createButton(const QString& text, const QIcon& icon, const
 }
 
 void Sidebar::updateButtonStyles(int targetFolderId) {
-    const QString normalStyle = "text-align: left; color: #9aa0a6; background-color: #21252b; border: none; padding: 10px; font-size: 14px; font-weight: 700;";
-    const QString hoverStyle = "text-align: left; color: #9aa0a6; background-color: #393d47; border: none; padding: 10px; font-size: 14px; font-weight: 700;";
+    const QString normalStyle =
+        "text-align: left;"
+        "color: #9aa0a6;"
+        "background-color: #21252b;"
+        "border: none;"
+        "padding: 10px;"
+        "font-size: 14px;"
+        "font-weight: 700;"
+    ;
+
+    const QString hoverStyle =
+        "text-align: left;"
+        "color: #9aa0a6;"
+        "background-color: #393d47;"
+        "border: none;"
+        "padding: 10px;"
+        "font-size: 14px;"
+        "font-weight: 700;"
+    ;
 
     allFilesButton->setStyleSheet(normalStyle);
     imagesButton->setStyleSheet(normalStyle);
